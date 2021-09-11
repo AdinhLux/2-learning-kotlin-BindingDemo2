ad188e6d74b9ff937e5b454ecee35b9bfc51ee0a
ADI <adinh@jiway.lu>
2021-09-11 16:48:58 +0200

    Student.kt : comment data class + 2nd constructor

--- app/src/main/java/com/anushka/bindingdemo3/Student.kt
+++ app/src/main/java/com/anushka/bindingdemo3/Student.kt
@@ -1,7 +1,26 @@
 package com.anushka.bindingdemo3
 
-data class Student (
-    var id:Int,
-    var name:String,
-    var email:String
-)
+/**
+ * Data classes specialize in holding data. The Kotlin compiler automatically generates the following functionality for them:
+ *
+ * A correct, complete, and readable 'toString()' method
+ * Value equality-based 'equals()' and 'hashCode()' methods
+ * Utility 'copy()' and 'componentN()' methods
+ *
+ * The primary constructor must not be empty, and it should contain only val or var properties.
+ *
+ * https://www.raywenderlich.com/20934302-deep-dive-into-kotlin-data-classes-for-android
+ * https://weihungchin.medium.com/a-practical-guide-to-kotlin-data-class-with-examples-6e30771ba717
+ */
+data class Student(
+    var id: Int,
+    var name: String,
+    var email: String
+) {
+
+    // Secondary Constructor (without email)
+    constructor(
+        id: Int,
+        name: String,
+    ) : this(id, name, "")
+}
